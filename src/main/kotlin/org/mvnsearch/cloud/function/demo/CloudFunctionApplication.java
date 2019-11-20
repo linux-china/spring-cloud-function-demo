4bbe0afb5d097f46cb05ccfbedd4af0c70c2cd82
linux_china <libing.chen@gmail.com>
2019-11-20 17:28:18 +0800

    cloud function app

---
+++ src/main/kotlin/org/mvnsearch/cloud/function/demo/CloudFunctionApplication.java
@@ -0,0 +1,16 @@
+package org.mvnsearch.cloud.function.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * cloud function app
+ *
+ * @author linux_china
+ */
+@SpringBootApplication
+public class CloudFunctionApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CloudFunctionApplication.class, args);
+    }
+}
