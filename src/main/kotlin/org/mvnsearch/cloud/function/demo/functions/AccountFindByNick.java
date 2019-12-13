0fb01d910feb05ff62b3271316d9b5b94ea79512
linux_china <libing.chen@gmail.com>
2019-12-13 17:11:14 +0800

    Account findByNick Function

---
+++ src/main/kotlin/org/mvnsearch/cloud/function/demo/functions/AccountFindByNick.java
@@ -0,0 +1,28 @@
+package org.mvnsearch.cloud.function.demo.functions;
+
+import org.mvnsearch.cloud.function.demo.domain.Account;
+import org.mvnsearch.cloud.function.demo.domain.AccountReactiveRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.stereotype.Controller;
+import reactor.core.publisher.Mono;
+
+import java.util.function.Function;
+
+
+/**
+ * Account findByNick Function
+ *
+ * @author linux_china
+ */
+@Controller
+public class AccountFindByNick implements Function<String, Mono<Account>> {
+    @Autowired
+    private AccountReactiveRepo accountRepo;
+
+    @Override
+    @MessageMapping("accountFindByNick")
+    public Mono<Account> apply(String nick) {
+        return accountRepo.findByNick(nick);
+    }
+}
