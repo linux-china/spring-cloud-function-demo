47aafd9dfed8903225adc03584c479afaf658d0c
linux_china <libing.chen@gmail.com>
2019-12-13 17:09:28 +0800

    polishing

--- src/main/kotlin/org/mvnsearch/cloud/function/demo/functions/Greeter.java
+++ src/main/kotlin/org/mvnsearch/cloud/function/demo/functions/Greeter.java
@@ -1,13 +1,10 @@
 package org.mvnsearch.cloud.function.demo.functions;
 
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.stereotype.Controller;
 import reactor.core.publisher.Mono;
 
 import java.util.function.Function;
-
-import static org.mvnsearch.cloud.function.demo.functions.Greeter.NAME;
 
 /**
  * Greeter function
@@ -15,13 +12,11 @@ import static org.mvnsearch.cloud.function.demo.functions.Greeter.NAME;
  * @author linux_china
  */
 @Controller
-@Qualifier(NAME)
 public class Greeter implements Function<String, Mono<String>> {
-    public final static String NAME = "greeter";
 
     @Override
-    @MessageMapping(NAME)
-    public Mono<String> apply(String s) {
-        return Mono.just("Hello " + s);
+    @MessageMapping("greeter")
+    public Mono<String> apply(String name) {
+        return Mono.just("Hello " + name);
     }
 }
