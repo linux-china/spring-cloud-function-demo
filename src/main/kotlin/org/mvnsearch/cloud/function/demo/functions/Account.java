6b7ae7f392aa45d0cd761215ddbb9d9d73ed9c89
linux_china <libing.chen@gmail.com>
2019-12-13 16:45:38 +0800

    Account

---
+++ src/main/kotlin/org/mvnsearch/cloud/function/demo/functions/Account.java
@@ -0,0 +1,81 @@
+package org.mvnsearch.cloud.function.demo.functions;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.relational.core.mapping.Column;
+import org.springframework.data.relational.core.mapping.Table;
+
+import java.time.LocalDateTime;
+
+/**
+ * Account
+ *
+ * @author linux_china
+ */
+@Table("account")
+public class Account {
+    @Id
+    private Integer id;
+    private String nick;
+    private String email;
+    private String phone;
+    @Column("passwd")
+    private String password;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNick() {
+        return nick;
+    }
+
+    public void setNick(String nick) {
+        this.nick = nick;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public LocalDateTime getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(LocalDateTime updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+}
