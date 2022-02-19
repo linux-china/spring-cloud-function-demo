e06ceff78fc595c4e983766cf2859245bac9bdb5
linux_china <libing.chen@gmail.com>
2022-02-19 11:32:12 -0800

    Add @Qualifier("accountFindByNick") for bean name

--- src/main/java/org/mvnsearch/cloud/function/demo/functions/AccountFindByNick.java
+++ src/main/java/org/mvnsearch/cloud/function/demo/functions/AccountFindByNick.java
@@ -3,6 +3,7 @@ package org.mvnsearch.cloud.function.demo.functions;
 import org.mvnsearch.cloud.function.demo.domain.Account;
 import org.mvnsearch.cloud.function.demo.domain.AccountReactiveRepo;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.stereotype.Controller;
 import reactor.core.publisher.Mono;
@@ -16,6 +17,7 @@ import java.util.function.Function;
  * @author linux_china
  */
 @Controller
+@Qualifier("accountFindByNick")
 public class AccountFindByNick implements Function<String, Mono<Account>> {
     @Autowired
     private AccountReactiveRepo accountRepo;
